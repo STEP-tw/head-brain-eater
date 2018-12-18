9833a6b16822dafb5e26ad22336042ed334262c4
brain-eater <34330361+brain-eater@users.noreply.github.com>
2018-12-18 12:53:08 +0530

    Refactor classifyParameters

--- src/parseInput.js
+++ src/parseInput.js
@@ -1,55 +1,67 @@
-const isNaturalNum = function(num) {
-  return !isNaN(num) && num > 0;
+const isWholelNum = function(num) {
+  return !isNaN(num) && num >= 0;
 };
 
 const isOptionProvided = function(arg) {
   return arg.startsWith("-");
 };
 
-const classifyParameters = function(parameters) {
-  let option = "n";
-  let firstArg = parameters[0];
-  let secondArg = parameters[1];
-  let firstFileNameIndex = 0;
-  let count = 10;
-
-  if (isOptionProvided(firstArg)) {
-    firstFileNameIndex++;
-    count = firstArg.slice(1);
-    if (!isNaturalNum(count)) {
-      option = firstArg[1];
-      count = firstArg.slice(2);
-      if (firstArg.length == 2) {
-        firstFileNameIndex++;
-        count = secondArg;
-      }
-    }
+const getOption = function(arg) {
+  if (!isWholelNum(arg[1])) {
+    return arg[1];
   }
-  let fileNames = parameters.slice(firstFileNameIndex);
-
-  return {
-    option,
-    count,
-    fileNames
-  };
+  return "n";
 };
+
+const getCount = function(args) {
+  let count = args[0].slice(1);
+
+  if (isWholelNum(count)) {
+    return count;
+  }
+  count = args[0].slice(2);
+  if (count != "") {
+    return count;
+  }
+
+  return args[1];
+};
+
+const getFileNames = function(args) {
+  if (args[0].length >= 3 || isWholelNum(args[0].slice(1))) {
+    return args.slice(1);
+  }
+  return args.slice(2);
+};
+
+const classifyParameters = function(parameters) {
+  if (isOptionProvided(parameters[0])) {
+    return parseParameters(parameters);
+  }
+  return { option: "n", count: "10", fileNames: parameters };
+};
+
+const parseParameters = function(parameters) {
+  let option = getOption(parameters[0]);
+  let count = getCount(parameters);
+  let fileNames = getFileNames(parameters);
+  return { option, count, fileNames };
+};
+
 const readFile = function(readFileSync, existsSync, fileName) {
-  let exists = true;
-  let name = fileName;
-  let content = "";
-  if (!existsSync(name)) {
+  if (!existsSync(fileName)) {
     exists = false;
     return {
-      name,
-      content,
-      exists
+      name: fileName,
+      content: "",
+      exists: false
     };
   }
-  content = readFileSync(fileName, "utf-8");
+  let content = readFileSync(fileName, "utf-8");
   return {
     content,
-    name,
-    exists
+    name: fileName,
+    exists: true
   };
 };
 
