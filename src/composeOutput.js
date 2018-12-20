cb9def7d622aee52403652f224895caf0c810f68
brain-eater <34330361+brain-eater@users.noreply.github.com>
2018-12-20 10:40:54 +0530

    Refactor composeOutput

--- src/composeOutput.js
+++ src/composeOutput.js
@@ -1,8 +1,15 @@
 const composeOutput = function(type, { name, content, exists }) {
   if (!exists) {
-    return type + ": " + name + ": No such file or directory";
+    return getErrorMessage(type, name);
   }
+  return getHeading(name, content);
+};
 
+const getErrorMessage = function(type, name) {
+  return type + ": " + name + ": No such file or directory";
+};
+
+const getHeading = function(name, content) {
   return "==> " + name + " <==\n" + content;
 };
 
