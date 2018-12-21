b9407b8e3d17f49490988d5be9c187d12917b4f1
brain-eater <34330361+brain-eater@users.noreply.github.com>
2018-12-21 10:59:51 +0530

    refactor Tail.js

--- tail.js
+++ tail.js
@@ -1,8 +1,8 @@
-const { readFileSync, existsSync } = require("fs");
+const fs = require("fs");
 const { tail } = require("./src/filter");
 
 const main = function() {
   let parameters = process.argv.slice(2);
-  console.log(tail(parameters, readFileSync, existsSync));
+  console.log(tail(parameters, fs));
 };
 main();
